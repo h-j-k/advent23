33968e21cb64bf470dd788f71ec41f072b2aa09d
H-J-K <han.junkwang@gmail.com>
2023-12-03 23:51:00 +0800

    Day 3 part 2.

--- src/advent23/day03.clj
+++ src/advent23/day03.clj
@@ -9,14 +9,18 @@
                 :points  (map (fn [x] {:x x :y y}) (range (. m start) (. m end)))}
                (lazy-seq (step))))))))
 
-(defn get-numbers-and-symbols [y s]
-  (let [numbers (get-groups #"\d+" y s)
-        symbols (get-groups #"[^0-9.]+" y s)]
-    (into numbers symbols)))
+(defn get-parts-and-symbols [y s]
+  (let [parts (get-groups #"\d+" y s) symbols (get-groups #"[^0-9.]+" y s)] (into parts symbols)))
 
-(defn explode [p]
+(defn get-parts-and-gears [y s]
+  (let [parts (get-groups #"\d+" y s) gears (get-groups #"\*" y s)] (into parts gears)))
+
+(defn process-with [f input]
+  (group-by (fn [group] (true? (:symbol? (:element group)))) (flatten (map-indexed f input))))
+
+(defn neighbors-of [p]
   (let [{x :x y :y} p]
-    (list
+    #{
       {:x (- x 1) :y (- y 1)},
       {:x x :y (- y 1)},
       {:x (+ x 1) :y (- y 1)},
@@ -25,11 +29,22 @@
       {:x (- x 1) :y (+ y 1)},
       {:x x :y (+ y 1)},
       {:x (+ x 1) :y (+ y 1)}
-      )))
+      }))
+
+(defn explode-all [symbols] (into #{} (mapcat neighbors-of (mapcat #(:points %) symbols))))
+
+(defn near-symbol? [part targets] (some #(contains? targets %) (:points part)))
+
+(defn number-of [part] (Integer/parseInt (:value (:element part))))
+
+(defn two-neighboring-parts-product [parts gear]
+  (let [neighboring-parts (filter #(near-symbol? % (explode-all [gear])) parts)]
+    (if (= (count neighboring-parts) 2) (reduce * (map number-of neighboring-parts)) 0)))
 
 (defn part1 [input]
-  (let [groups (flatten (map-indexed get-numbers-and-symbols input))]
-    (let [{numbers false symbols true} (group-by (fn [group] (true? (:symbol? (:element group)))) groups)]
-      (let [neighboring-symbols (into #{} (flatten (map explode (flatten (map #(:points %) symbols)))))]
-        (reduce + (map (fn [n] (Integer/parseInt (:value (:element n))))
-                       (filter (fn [n] (some #(contains? neighboring-symbols %) (:points n))) numbers)))))))
+  (let [{parts false symbols true} (process-with get-parts-and-symbols input) targets (explode-all symbols)]
+    (reduce + (map number-of (filter #(near-symbol? % targets) parts)))))
+
+(defn part2 [input]
+  (let [{parts false gears true} (process-with get-parts-and-gears input)]
+    (reduce + (map #(two-neighboring-parts-product parts %) gears))))
