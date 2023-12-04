7609eb0f8cd443fb480d66474f16dbd4cd933114
H-J-K <han.junkwang@gmail.com>
2023-12-04 10:22:07 +0800

    Day 2 refactoring.

--- src/advent23/day02.clj
+++ src/advent23/day02.clj
@@ -5,18 +5,16 @@
   (and (<= (:r this) (:r other)) (<= (:g this) (:g other)) (<= (:b this) (:b other))))
 
 (defn least-number [game]
-  (let [least-of (fn [a b] {:r (max (:r a) (:r b)) :g (max (:g a) (:g b)) :b (max (:b a) (:b b))})
-        least (reduce least-of {:r 0 :g 0 :b 0} (:sets game))]
-    (reduce * [(:r least) (:g least) (:b least)])))
+  (let [least-of (fn [a b] {:r (max (:r a) (:r b)) :g (max (:g a) (:g b)) :b (max (:b a) (:b b))})]
+    (reduce * (vals (reduce least-of {:r 0 :g 0 :b 0} (:sets game))))))
 
 (defn to-cube-set [s]
   (let [parse (fn [re] (parse-long (or (last (re-find re s)) "0")))]
     {:r (parse #"(\d+) r") :g (parse #"(\d+) g") :b (parse #"(\d+) b")}))
 
 (defn to-game [s]
-  (let [[_ id raw-sets] (re-matches #"Game (\d+): (.+)" s)
-        sets (map to-cube-set (str/split raw-sets #"; "))]
-    {:id (parse-long id) :sets sets}))
+  (let [[_ id sets] (re-matches #"Game (\d+): (.+)" s)]
+    {:id (parse-long id) :sets (map to-cube-set (str/split sets #"; "))}))
 
 (defn part1 [input target]
   (let [is-in (fn [game target] (every? #(&<= % target) (:sets game)))]
