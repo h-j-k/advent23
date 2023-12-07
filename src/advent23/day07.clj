64c9fba2df57e9af6ef9e7481da220aab41d7e92
H-J-K <han.junkwang@gmail.com>
2023-12-08 01:37:59 +0800

    Day 7 part 2.

--- src/advent23/day07.clj
+++ src/advent23/day07.clj
@@ -3,9 +3,11 @@
 
 (def hand-types [:high :pair :two-pair :three-of-kind :full-house :four-of-kind :five-of-kind])
 
-(def rank "23456789TJQKA")
+(def part-1-rank "23456789TJQKA")
 
-(defn get-type [hand]
+(def part-2-rank "J23456789TQKA")
+
+(defn part-1-type [hand]
   (let [counts (sort > (vals (frequencies (:hand hand))))]
     (cond
       (= counts '(5)) :five-of-kind
@@ -17,22 +19,37 @@
       (= counts '(1 1 1 1 1)) :high
       )))
 
-(defn sort-hands [a b]
-  (loop [[a-char & rest-a] a [b-char & rest-b] b]
-    (let [a-rank (str/index-of rank a-char) b-rank (str/index-of rank b-char)]
-      (cond
-        (< a-rank b-rank) -1
-        (> a-rank b-rank) 1
-        :else (recur rest-a rest-b)
-        ))))
+(defn part-2-type [hand]
+  (if (nil? (str/index-of (:hand hand) "J"))
+    (part-1-type hand)
+    (last
+      (sort-by
+        #(.indexOf hand-types %)
+        (map part-1-type
+             (map #(assoc hand :hand (str/replace (:hand hand) "J" (str %))) (drop 1 part-2-rank)))))))
 
-(defn sort-all-hands [hand-and-bids]
-  (run! println (map (comp #(sort-by :hand sort-hands %) val) (sort-by #(.indexOf hand-types (key %)) (group-by get-type hand-and-bids))))
-  (flatten
-    (map (comp #(sort-by :hand sort-hands %) val)
-         (sort-by #(.indexOf hand-types (key %)) (group-by get-type hand-and-bids)))))
+(defn sort-hands [rank]
+  (fn [a b]
+    (loop [[a-char & rest-a] a [b-char & rest-b] b]
+      (let [a-rank (str/index-of rank a-char) b-rank (str/index-of rank b-char)]
+        (cond
+          (< a-rank b-rank) -1
+          (> a-rank b-rank) 1
+          :else (recur rest-a rest-b)
+          )))))
 
 (defn to-hand-and-bids [line] (zipmap [:hand :bid] (into [] (str/split line #"\s+"))))
 
-(defn part1 [input]
-  (reduce + (map-indexed #(* (inc %1) (parse-long (:bid %2))) (sort-all-hands (map to-hand-and-bids input)))))
+(defn process [input get-type rank]
+  (let [hand-and-bids (map to-hand-and-bids input)]
+    (reduce
+      +
+      (map-indexed
+        #(* (inc %1) (parse-long (:bid %2)))
+        (flatten
+          (map (comp #(sort-by :hand (sort-hands rank) %) val)
+               (sort-by #(.indexOf hand-types (key %)) (group-by get-type hand-and-bids))))))))
+
+(defn part1 [input] (process input part-1-type part-1-rank))
+
+(defn part2 [input] (process input part-2-type part-2-rank))
--- test/advent23/day07_test.clj
+++ test/advent23/day07_test.clj
@@ -1,16 +1,9 @@
 (ns advent23.day07-test
-  (:require [advent23.day07 :refer [part1]]
+  (:require [advent23.day07 :refer :all]
             [clojure.test :refer :all]))
 
 (def input (clojure.string/split-lines (slurp "resources/day07.txt")))
 
-(def test-input
-  [
-   "32T3K 765",
-   "QQQJA 483",
-   "T55J5 684",
-   "KK677 28",
-   "KTJJT 220"
-   ])
-
 (deftest part1-test (testing "Part 1" (is (= (part1 input) 251287184))))
+
+(deftest part2-test (testing "Part 2" (is (= (part2 input) 5905))))
