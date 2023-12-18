4b3a1dd34954f24f0b8f38ea26abcb4e995b7045
H-J-K <han.junkwang@gmail.com>
2023-12-19 01:01:56 +0800

    Day 18 part 2.

--- src/advent23/day18.clj
+++ src/advent23/day18.clj
@@ -1,15 +1,22 @@
 (ns advent23.day18)
 
 (defn calculate-area [{:keys [x1 y1 area vertices]} {:keys [direction delta]}]
-  (let [delta-value (parse-long delta)
-        x2 (cond (= "L" direction) (- x1 delta-value) (= "R" direction) (+ x1 delta-value) :else x1)
-        y2 (cond (= "U" direction) (+ y1 delta-value) (= "D" direction) (- y1 delta-value) :else y1)]
+  (let [x2 (cond (= "L" direction) (- x1 delta) (= "R" direction) (+ x1 delta) :else x1)
+        y2 (cond (= "U" direction) (+ y1 delta) (= "D" direction) (- y1 delta) :else y1)]
     {:x1 x2 :y1 y2 :area (+ area (* x1 y2) (- (* x2 y1))) :vertices (+ vertices (abs (- x1 x2)) (abs (- y1 y2)))}))
 
-(defn calculate-area-with-vertices [plans]
-  (let [{:keys [area vertices]} (reduce calculate-area {:x1 0 :y1 0 :area 0 :vertices 0} plans)]
+(defn process [parser input]
+  (let [{:keys [area vertices]} (reduce calculate-area {:x1 0 :y1 0 :area 0 :vertices 0} (map parser input))]
     (str (+ 1 (abs (/ area 2)) (/ vertices 2)))))
 
-(defn parse [line] (zipmap [:direction :delta] (rest (re-matches #"^([LRUD]) (\d+) .*$" line))))
+(defn parse-plan [line]
+  (let [[_ direction delta] (re-find #"^([LRUD]) (\d+)" line)]
+    (zipmap [:direction :delta] [direction (parse-long delta)])))
 
-(defn part1 [input] (calculate-area-with-vertices (map parse input)))
+(defn part1 [input] (process parse-plan input))
+
+(defn parse-color [line]
+  (let [[_ hex-delta direction] (re-find #"#([0-9a-f]{5})([0-3]).$" line)]
+    (zipmap [:direction :delta] [(str (nth "RDLU" (parse-long direction))) (Long/parseLong hex-delta 16)])))
+
+(defn part2 [input] (process parse-color input))
--- test/advent23/day18_test.clj
+++ test/advent23/day18_test.clj
@@ -1,9 +1,11 @@
 (ns advent23.day18-test
-  (:require [clojure.test :refer :all])
-  (:require [advent23.day18 :refer [part1]]))
+  (:require [advent23.day18 :refer :all]
+            [clojure.test :refer :all]))
 
 (def input (clojure.string/split-lines (slurp "resources/day18.txt")))
 
 (def answers (clojure.string/split-lines (slurp "resources/day18-answers.txt")))
 
 (deftest part1-test (testing "Part 1" (is (= (part1 input) (first answers)))))
+
+(deftest part2-test (testing "Part 2" (is (= (part2 input) (second answers)))))
