adbf4f45e058b198c1fceebc0f96fef547038e09
H-J-K <han.junkwang@gmail.com>
2023-12-22 10:59:55 +0800

    Day 20 part 1 refactoring.

--- src/advent23/day20.clj
+++ src/advent23/day20.clj
@@ -20,6 +20,10 @@
     (= (:type module) "&") (handle-& module pulse source)
     :else [module []]))
 
+(defn handle-next [[modules pulses targets] [pulse source target]]
+  (let [[updated next] (handle (get modules target {:name target}) pulse source)]
+    [(assoc modules (:name updated) updated) (update pulses pulse + 1) (concat targets next)]))
+
 (defn parse-row [row]
   (let [[_ type name targets] (re-find #"^([%&])*([^ ]+) -> (.+)$" row)]
     {:type type :name name :targets (str/split targets #", ")}))
@@ -37,20 +41,13 @@
   (let [modules-list (map parse-row input) modules (zipmap (map #(:name %) modules-list) modules-list)]
     (into {} (map #(enrich modules %) modules-list))))
 
-(defn process [modules pulses to-process]
+(defn process [[modules pulses to-process]]
   (if (empty? to-process)
-    (do (prn pulses) [modules pulses])
-    (do
-      (let [[updated-modules next-targets]
-            (reduce (fn [[acc next] [pulse source target]]
-                      (let [[updated targets] (handle (get acc target {:name target}) pulse source)]
-                        [(assoc acc (:name updated) updated) (concat next targets)]))
-                    [modules []]
-                    to-process)]
-        (recur updated-modules (merge-with + pulses (frequencies (map first to-process))) next-targets)))))
+    [modules pulses]
+    (recur (reduce handle-next [modules pulses []] to-process))))
 
 (defn part1 [input]
-  (loop [i 0 [modules pulses] [(parse input) {}]]
+  (loop [i 0 [modules pulses] [(parse input) {:low 0 :high 0}]]
     (if (= i 1000)
       (str (apply * (.values pulses)))
-      (recur (inc i) (process modules pulses [[:low "button" "broadcaster"]])))))
+      (recur (inc i) (process [modules pulses [[:low "button" "broadcaster"]]])))))
